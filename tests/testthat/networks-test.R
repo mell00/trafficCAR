09a7898ad892fd984262f685ea7c00bd2c1c6125
Madison Ell <mellmell2550@gmail.com>
2025-12-30 09:21:28 -0800

    Add T-junction toy network

--- tests/testthat/networks-test.R
+++ tests/testthat/networks-test.R
@@ -9,3 +9,13 @@ toy_L <- sf::st_sf(
   crs = 4326
 )
 
+# T-junction network (3 segments, 4 nodes: one degree-3 intersection)
+
+toy_T <- sf::st_sf(
+  geometry = sf::st_sfc(
+    sf::st_linestring(matrix(c(0, 0, 1, 0), ncol = 2, byrow = TRUE)),
+    sf::st_linestring(matrix(c(1, -1, 1, 0), ncol = 2, byrow = TRUE)),
+    sf::st_linestring(matrix(c(1, 0, 1, 1), ncol = 2, byrow = TRUE))
+  ),
+  crs = 4326
+)

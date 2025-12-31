db804f20471c7ea5926b6972237db99ad5ef32b0
Madison Ell <mellmell2550@gmail.com>
2025-12-31 04:50:46 -0800

    Fix expected output bugs

--- R/networks.R
+++ R/networks.R
@@ -21,35 +21,40 @@ build_network <- function(roads_sf, crs_out = 3857) {
   roads <- sf::st_transform(roads, crs_out)
   roads <- sf::st_cast(roads, "LINESTRING")
 
-  # node intersections (sf-stable pattern)
+  # node intersections (sf-stable)
   geom <- sf::st_union(sf::st_geometry(roads))
   geom <- sf::st_node(geom)
   geom <- sf::st_cast(geom, "LINESTRING")
-
   roads <- sf::st_sf(geometry = geom, crs = sf::st_crs(roads))
 
-  # create nodes
-  nodes <- sf::st_as_sf(sf::st_cast(sf::st_union(roads), "POINT"))
+  # nodes (unique by geometry)
+  node_geom <- sf::st_cast(sf::st_union(roads), "POINT")
+  coords <- sf::st_coordinates(node_geom)
+  node_geom <- node_geom[!duplicated(coords), ]
+
+  nodes <- sf::st_sf(geometry = node_geom, crs = sf::st_crs(roads))
   nodes$node_id <- seq_len(nrow(nodes))
 
-  # create edges
+  # edges
   edges <- roads
-  edges$from <- sf::st_nearest_feature(
-    sf::st_startpoint(edges), nodes
+
+  endpts <- sf::st_cast(
+    sf::st_line_sample(edges, sample = c(0, 1)),
+    "POINT"
   )
-  edges$to <- sf::st_nearest_feature(
-    sf::st_endpoint(edges), nodes
-  )
+
+  edges$from <- sf::st_nearest_feature(endpts[c(TRUE, FALSE)], nodes)
+  edges$to   <- sf::st_nearest_feature(endpts[c(FALSE, TRUE)], nodes)
   edges$length <- sf::st_length(edges)
 
-  # create graph
-  g <- igraph::graph_from_data_frame(
-    edges[, c("from", "to")],
-    directed = FALSE,
-    vertices = nodes
+  # graph (integer node IDs)
+  edge_mat <- as.matrix(
+    sf::st_drop_geometry(edges[, c("from", "to")])
   )
 
-  # create adjacency matrix
+  g <- igraph::graph_from_edgelist(edge_mat, directed = FALSE)
+
+  # adjacency
   A <- igraph::as_adjacency_matrix(g, sparse = TRUE)
 
   list(

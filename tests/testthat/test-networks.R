d2ef35b01e9dc05d5c0742c62e844885a24cc0f4
Madison Ell <mellmell2550@gmail.com>
2025-12-31 05:04:36 -0800

    Add L toy network expected topology test

--- tests/testthat/test-networks.R
+++ tests/testthat/test-networks.R
@@ -7,3 +7,16 @@ test_that("build_network returns expected components", {
     c("roads", "nodes", "edges", "graph", "A")
   )
 })
+
+test_that("L-shaped network has expected topology", {
+  net <- build_network(toy_L)
+
+  expect_equal(nrow(net$nodes), 3)
+  expect_equal(nrow(net$edges), 2)
+  expect_true(igraph::is_connected(net$graph))
+
+  deg <- sort(igraph::degree(net$graph))
+  expect_equal(deg, c(1, 1, 2))
+})
+
+

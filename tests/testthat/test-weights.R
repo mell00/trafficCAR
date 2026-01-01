052b1c202f70b59015d8948ac62da0e6420dcc6e
Madison Ell <mellmell2550@gmail.com>
2026-01-01 13:27:44 -0800

    Add NA input check for as_sparse_adjacency

--- tests/testthat/test-weights.R
+++ tests/testthat/test-weights.R
@@ -4,7 +4,14 @@ test_that("as_sparse_adjacency rejects NULL and non-matrices", {
   expect_error(as_sparse_adjacency(list(a = 1)))
 })
 
+
 test_that("as_sparse_adjacency rejects non-square matrices", {
   A <- matrix(1, nrow = 3, ncol = 2)
+  expect_error(as_sparse_adjacency(A))
+})
+
+
+test_that("as_sparse_adjacency rejects NA values", {
+  A <- matrix(c(0, 1, NA, 0), 2, 2)
   expect_error(as_sparse_adjacency(A))
 })

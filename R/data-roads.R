0ea6f84d758d337996ad3529d9543b93504fb1bd
Madison Ell <mellmell2550@gmail.com>
2026-01-05 16:10:22 -0800

    Add dataset documentation to NAMESPACE

---
+++ R/data-roads.R
@@ -0,0 +1,22 @@
+#' Example road network datasets
+#'
+#' A collection of example road network datasets provided as `sf` objects.
+#' These datasets are intended for demonstration, testing, and benchmarking.
+#'
+#' Each object contains LINESTRING road geometries suitable for use with
+#' `roads_to_segments()` and related functions.
+#'
+#' @format An object of class `sf` with LINESTRING geometries.
+#'
+#' @details
+#' Included datasets:
+#' \describe{
+#'   \item{roads_small}{Small road network example.}
+#'   \item{roads_midsize}{Medium-sized road network example.}
+#'   \item{roads_madison}{Road network for Madison, WI.}
+#' }
+#'
+#' @name roads_datasets
+#' @docType data
+#' @keywords datasets
+NULL

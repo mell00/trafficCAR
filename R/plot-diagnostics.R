734cfcfd873204e569dcf97a39b261ea7e4d2a08
Madison Ell <mellmell2550@gmail.com>
2026-01-07 18:25:33 -0800

    Implement plot_mcmc_diagnostics

---
+++ R/plot-diagnostics.R
@@ -0,0 +1,8 @@
+#' MCMC diagnostic plots
+#'
+#' @param fit traffic_fit
+#' @export
+plot_mcmc_diagnostics <- function(fit) {
+  ess <- vapply(fit$draws, posterior::ess_basic, numeric(1))
+  data.frame(parameter = names(ess), ess = ess)
+}

9ab837a361885a34051d43ca490ff21efc889816
Madison Ell <mellmell2550@gmail.com>
2026-01-07 18:20:56 -0800

    Implement plot_relative_congestion (relative congestion on road network accounting for covariates)

--- R/plot-spatial.R
+++ R/plot-spatial.R
@@ -18,3 +18,30 @@ plot_predicted <- function(fit, roads) {
       name = fit$outcome_label
     )
 }
+
+
+
+#' Plot relative congestion on road network
+#'
+#' Shows systematic deviations after accounting for covariates.
+#'
+#' @param fit traffic_fit
+#' @param roads sf
+#' @return ggplot
+#' @export
+plot_relative_congestion <- function(fit, roads) {
+  x_mean <- colMeans(fit$draws$x)
+  rel <- x_mean / sd(x_mean)
+
+  roads$relative_congestion <- rel
+
+  ggplot2::ggplot(roads) +
+    ggplot2::geom_sf(
+      ggplot2::aes(color = relative_congestion),
+      linewidth = 1
+    ) +
+    ggplot2::scale_color_gradient2(
+      low = "blue", mid = "white", high = "red",
+      name = "Relative congestion\n(vs city average)"
+    )
+}

3b6a86a8d3c17478e1c35bfec808cf2801354d3c
Madison Ell <mellmell2550@gmail.com>
2026-01-08 18:27:25 -0800

    Import posterior package

--- R/plot-diagnostics.R
+++ R/plot-diagnostics.R
@@ -1,6 +1,7 @@
 #' MCMC diagnostic plots
 #'
 #' @param fit traffic_fit
+#' @importFrom posterior ess_basic
 #' @export
 plot_mcmc_diagnostics <- function(fit) {
   ess <- vapply(fit$draws, posterior::ess_basic, numeric(1))

489108a032b583290b63d226c62351da8b5f43ae
Madison Ell <mellmell2550@gmail.com>
2026-01-08 20:01:31 -0800

    Add plot_roads_static value validation test

--- tests/testthat/test-plot-static.R
+++ tests/testthat/test-plot-static.R
@@ -7,3 +7,24 @@ test_that("plot_roads_static validates sf_aug is sf", {
     ignore.case = TRUE
   )
 })
+
+
+
+test_that("plot_roads_static validates value choices via match.arg", {
+  sf_aug <- sf::st_sf(
+    predicted_speed = 10,
+    predicted_volume = 100,
+    relative_congestion = 0.2,
+    geometry = sf::st_sfc(
+      sf::st_linestring(matrix(c(0, 0, 1, 0), ncol = 2, byrow = TRUE))
+    ),
+    crs = 4326
+  )
+
+  expect_error(
+    trafficCAR::plot_roads_static(sf_aug, value = "nope"),
+    "should be one of|one of",
+    ignore.case = TRUE
+  )
+})
+

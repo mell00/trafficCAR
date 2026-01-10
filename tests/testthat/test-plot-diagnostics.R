7d224bd7bc18ee8d7a58766ed6b0f58d0a1f7254
Madison Ell <mellmell2550@gmail.com>
2026-01-10 11:17:19 -0800

    Add non list draw safeguard test

--- tests/testthat/test-plot-diagnostics.R
+++ tests/testthat/test-plot-diagnostics.R
@@ -66,3 +66,9 @@ test_that("plot_mcmc_diagnostics rejects missing draws element", {
   fit <- structure(list(), class = "traffic_fit")
   expect_error(plot_mcmc_diagnostics(fit), "draws|\\$draws|subscript|NULL", ignore.case = TRUE)
 })
+
+
+test_that("plot_mcmc_diagnostics errors when draws is not a list", {
+  fit <- structure(list(draws = rnorm(10)), class = "traffic_fit")
+  expect_error(plot_mcmc_diagnostics(fit), "must be a list", ignore.case = TRUE)
+})

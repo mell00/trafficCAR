2b3bedcd2486774d4f213999572b9896b6d201c1
Madison Ell <mellmell2550@gmail.com>
2026-01-10 11:17:35 -0800

    Add non list fit$draws safeguard

--- R/plot-diagnostics.R
+++ R/plot-diagnostics.R
@@ -5,6 +5,7 @@
 #' @export
 plot_mcmc_diagnostics <- function(fit) {
   if (is.null(fit$draws)) stop("`fit$draws` is missing.")
+  if (!is.list(fit$draws)) stop("`fit$draws` must be a list.")
   ess <- vapply(fit$draws, posterior::ess_basic, numeric(1))
   data.frame(parameter = names(ess), ess = ess)
 }

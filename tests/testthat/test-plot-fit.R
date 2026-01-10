30fdbc528b67d366de3ac3c1e5ae7e0ff92fb4b1
Madison Ell <mellmell2550@gmail.com>
2026-01-10 06:59:51 -0800

    Add fit class and data type validation tests

--- tests/testthat/test-plot-fit.R
+++ tests/testthat/test-plot-fit.R
@@ -23,3 +23,30 @@ test_that("plot_observed_fitted returns a ggplot with correct labels and data", 
   expect_equal(p$data$observed, c(10, 20))
   expect_equal(p$data$predicted, colMeans(fit$draws$mu))
 })
+
+
+
+test_that("plot_observed_fitted validates fit class and data type", {
+  skip_if_not_installed("ggplot2")
+
+  fit <- list(
+    draws = list(mu = matrix(1, nrow = 2, ncol = 2)),
+    outcome_col = "speed",
+    outcome_label = "Speed"
+  )
+  data <- data.frame(speed = c(1, 2))
+
+  expect_error(
+    plot_observed_fitted(fit, data),
+    "`fit` must be a `traffic_fit`",
+    fixed = TRUE
+  )
+
+  class(fit) <- "traffic_fit"
+  expect_error(
+    plot_observed_fitted(fit, as.list(data)),
+    "`data` must be a data.frame",
+    fixed = TRUE
+  )
+})
+

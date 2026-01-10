087fd53a665cacfaee795ae93fca442c6276f675
Madison Ell <mellmell2550@gmail.com>
2026-01-10 11:11:26 -0800

    Add missing fit$draws safeguard

--- R/plot-diagnostics.R
+++ R/plot-diagnostics.R
@@ -4,6 +4,7 @@
 #' @importFrom posterior ess_basic
 #' @export
 plot_mcmc_diagnostics <- function(fit) {
+  if (is.null(fit$draws)) stop("`fit$draws` is missing.")
   ess <- vapply(fit$draws, posterior::ess_basic, numeric(1))
   data.frame(parameter = names(ess), ess = ess)
 }

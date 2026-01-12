f09bde16f9d24e5ad1e9351cd51c6f3c5083b993
Madison Ell <mellmell2550@gmail.com>
2026-01-11 22:45:01 -0800

    Add check that character place resolves bbox and queries correctly

--- tests/testthat/test-osm.R
+++ tests/testthat/test-osm.R
@@ -89,3 +89,44 @@ test_that("fetch_osm_roads accepts character place and passes bbox through opq()
   expect_identical(got_features[[1L]]$value, c("primary", "secondary"))
 })
 
+
+test_that("fetch_osm_roads accepts bbox object input and does not call getbb()", {
+  skip_if_not_installed("osmdata")
+  skip_if_not_installed("sf")
+
+  bbox <- matrix(
+    c(-0.2, 51.4, 0.2, 51.6),
+    nrow = 2,
+    dimnames = list(c("x", "y"), c("min", "max"))
+  )
+
+  getbb_called <- 0L
+  got_bbox <- NULL
+
+  roads_sf <- sf::st_sf(
+    osm_id = 1,
+    geometry = sf::st_sfc(sf::st_linestring(rbind(c(0, 0), c(1, 0)))),
+    crs = 4326
+  )
+
+  local_mocked_bindings(
+    osm_getbb = function(...) {
+      getbb_called <<- getbb_called + 1L
+      bbox
+    },
+    osm_opq = function(bbox_arg) {
+      got_bbox <<- bbox_arg
+      structure(list(bbox = bbox_arg), class = "opq")
+    },
+    osm_add_feature = function(q, key, value = NULL) q,
+    osm_sf = function(q, quiet = TRUE, ...) list(osm_lines = roads_sf),
+    .env = asNamespace("trafficCAR")
+  )
+
+  res <- fetch_osm_roads(bbox, layer = "osm_lines")
+
+  expect_s3_class(res, "sf")
+  expect_identical(res, roads_sf)
+  expect_identical(got_bbox, bbox)
+  expect_identical(getbb_called, 0L)
+})

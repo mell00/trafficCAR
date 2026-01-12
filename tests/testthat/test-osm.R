89177a972626dd97f9a81171d998f374f243d654
Madison Ell <mellmell2550@gmail.com>
2026-01-11 22:42:18 -0800

    Add character place length validation test

--- tests/testthat/test-osm.R
+++ tests/testthat/test-osm.R
@@ -11,4 +11,26 @@ test_that("fetch_osm_roads errors if osmdata is not installed", {
 })
 
 
+test_that("fetch_osm_roads validates `place` when character", {
+  skip_if_not_installed("osmdata")
+  skip_if_not_installed("sf")
+
+  expect_error(
+    fetch_osm_roads(c("A", "B")),
+    "`place` must be a single place name",
+    fixed = TRUE
+  )
+
+  # unresolved place -> NULL bbox
+  local_mocked_bindings(
+    osm_getbb = function(place) NULL,
+    .env = asNamespace("trafficCAR")
+  )
+
+  expect_error(
+    fetch_osm_roads("this is not a real place probably"),
+    "Unable to resolve `place` to a bounding box",
+    ignore.case = FALSE
+  )
+})
 
